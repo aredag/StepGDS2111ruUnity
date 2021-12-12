b7679aa4938a57a89356d91e2896ccc12eb09416
TheSerjio <serggol2006@gmail.com>
2021-12-12 11:05:02 +0200

    ui

--- Assets/SerjioWorks/Time/Clock.cs
+++ Assets/SerjioWorks/Time/Clock.cs
@@ -4,13 +4,14 @@ namespace TimeClock
 {
     public class Clock : MonoBehaviour
     {
+        [SerializeField] private bool UTC;
         [SerializeField] private Transform hours;
         [SerializeField] private Transform minutes;
         [SerializeField] private Transform seconds;
 
         private void Update()
         {
-            var now = System.DateTime.Now;
+            var now = UTC ? System.DateTime.UtcNow : System.DateTime.Now;
             hours.transform.localEulerAngles = Vector3.back * now.Hour * 15f;
             minutes.transform.localEulerAngles = Vector3.back * now.Minute * 6f;
             seconds.transform.localEulerAngles = Vector3.back * now.Second * 6f;
